25a3114e6ea24e8463a8ad2dc3da49cc318c0c22
NikitaFeodonit <nikitafeodonit@yandex.com>
2015-07-25 20:27:20 +0300

    Copyright

--- app/src/main/java/com/jbak/ctrl/SameThreadTimer.java
+++ app/src/main/java/com/jbak/ctrl/SameThreadTimer.java
@@ -1,3 +1,25 @@
+/*
+ * Project:  Jbak Keyboard Mod
+ * Purpose:  Keyboard for Android
+ * Author:   Yuriy Bakunin (aka jtest), jbak1979@gmail.com, http://jbak.ru/jbakkeyboard/
+ * Modification:  NikitaFeodonit, nfeodonit@yandex.com
+ * *****************************************************************************
+ * Copyright (C) 2007-2015 jbak.ru
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.jbak.ctrl;
 
 import android.os.Handler;
